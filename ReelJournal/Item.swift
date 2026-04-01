ca577af0fc5149dc9dc9ffe26e0d3ef04ba3bba3
Dom Scafati <domscafati@hotmail.com>
2026-04-01 11:59:44 -0500

    Initial Commit

---
+++ ReelJournal/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  ReelJournal
+//
+//  Created by Dom S on 4/1/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ ReelJournalTests/ReelJournalTests.swift
@@ -0,0 +1,17 @@
+//
+//  ReelJournalTests.swift
+//  ReelJournalTests
+//
+//  Created by Dom S on 4/1/26.
+//
+
+import Testing
+@testable import ReelJournal
+
+struct ReelJournalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
