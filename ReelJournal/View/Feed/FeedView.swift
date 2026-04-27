3c6e76804d8e139c0f6e374afffe3fd4c5cd8f98
Dom Scafati <domscafati@hotmail.com>
2026-04-27 16:41:45 -0500

    added loadEntries() call onAppear for the FeedView. issue #19.

--- ReelJournal/View/Feed/FeedView.swift
+++ ReelJournal/View/Feed/FeedView.swift
@@ -62,6 +62,8 @@ struct FeedView: View {
             if viewModel == nil {
                 viewModel = FeedViewModel(repository: feedRepository)
             }
+            
+            viewModel?.loadEntries()
         }
     }
 }
